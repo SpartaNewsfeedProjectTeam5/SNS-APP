a675e5b66d4268d8fe4919ad903cd12feca49a5e
kisu kim <qpsrltn@gmail.com>
2025-08-19 17:32:44 +0900

    init commit

---
+++ src/test/java/org/example/snsapp/SnsAppApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.snsapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SnsAppApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
