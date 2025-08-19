493a4fd0149dae97de0968d4e7a277bbb446a404
Hyjpark <hyej042@gmail.com>
2025-08-19 21:14:22 +0900

    feat: User 엔티티 필드 정의

--- src/main/java/org/example/snsapp/domain/user/entity/User.java
+++ src/main/java/org/example/snsapp/domain/user/entity/User.java
@@ -1,8 +1,28 @@
 package org.example.snsapp.domain.user.entity;
 
-import jakarta.persistence.Entity;
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
 import org.example.snsapp.global.entity.AuditableEntity;
 
+@Getter
 @Entity
+@NoArgsConstructor
 public class User extends AuditableEntity {
+
+    @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(unique = true)
+    private String email;
+
+    private String password;
+
+    private String userName;
+
+    private int age;
+
+    private boolean isDeleted;
+
+    private String profileImage;
 }
