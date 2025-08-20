6a72df4441194dd8d422508b4e02a6d4d6f14663
Hyjpark <hyej042@gmail.com>
2025-08-20 19:26:36 +0900

    feat: 프로필 수정 구현
    - 요청 데이터 검증 @Valid 적용
    - UserUpdateResponse DTO 반환
    - followerCount, followingCount 임시 하드코딩
    - userId 임시 하드코딩 (세션 미구현)

--- src/main/java/org/example/snsapp/domain/user/controller/UserController.java
+++ src/main/java/org/example/snsapp/domain/user/controller/UserController.java
@@ -1,13 +1,13 @@
 package org.example.snsapp.domain.user.controller;
 
+import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.example.snsapp.domain.user.dto.UserBaseResponse;
+import org.example.snsapp.domain.user.dto.UserUpdateRequest;
+import org.example.snsapp.domain.user.dto.UserUpdateResponse;
 import org.example.snsapp.domain.user.service.UserService;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/api/newsfeeds/v1")
@@ -16,9 +16,14 @@ public class UserController {
 
     private final UserService userService;
 
-
     @GetMapping("/users/{email}/profile")
     public ResponseEntity<UserBaseResponse> getUserProfile(@PathVariable String email) {
         return ResponseEntity.ok(userService.getUserProfile(email));
+    }
+
+    @PutMapping("/users/me/profile")
+    public ResponseEntity<UserUpdateResponse> updateUserProfile(@Valid @RequestBody UserUpdateRequest dto) {
+        //  TODO: 인증 정보로 로그인 사용자 ID 가져오기
+        return ResponseEntity.ok(userService.updateUserProfile(dto));
     }
 }
