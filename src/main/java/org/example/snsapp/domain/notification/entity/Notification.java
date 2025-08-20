1cb726bfcc197df3d2d6349f5f87ef028e364dbd
eunseo04 <jes040616@naver.com>
2025-08-20 13:14:38 +0900

    Notification 엔티티 추가

---
+++ src/main/java/org/example/snsapp/domain/notification/entity/Notification.java
@@ -0,0 +1,39 @@
+package org.example.snsapp.domain.notification.entity;
+
+import jakarta.persistence.*;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import org.example.snsapp.domain.notification.Enum.NotificationContentType;
+
+@Entity
+@Getter
+@Table(name="notification")
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class Notification extends BaseEntity{
+
+    @ManyToOne(fetch=FetchType.LAZY, optional=false)
+    @JoinColumn(name = "from_id")
+    @Column(nullable=false)
+    private User fromUser; //보낸 유저
+
+    @ManyToOne(fetch=FetchType.LAZY, optional=false)
+    @JoinColumn(name = "to_id")
+    @Column(nullable=false)
+    private User toUser; //받는 유저
+
+    @Column(nullable=false, length=30)
+    @Enumerated(EnumType.STRING)
+    private NotificationContentType type; //타입
+
+    @Column(nullable=false, length = 250)
+    private String message; //메세지
+
+    public NotificationEntity(User fromUser, User toUser, NotificationContentType type, String message) {
+        this.fromUser = fromUser;
+        this.toUser = toUser;
+        this.type = type;
+        this.message = message;
+    }
+}
+
