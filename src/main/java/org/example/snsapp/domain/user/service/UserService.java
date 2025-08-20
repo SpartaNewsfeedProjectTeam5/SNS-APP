5e70d85e866b2d3369ba9fb34bb2f6a980a11e80
Hyjpark <hyej042@gmail.com>
2025-08-20 16:38:54 +0900

    feat: 프로필 조회 구현
    - UserBaseResponse DTO 반환
    - followerCount, followingCount는 팔로우 기능 미구현으로 임시 하드코딩 값 사용

--- src/main/java/org/example/snsapp/domain/user/service/UserService.java
+++ src/main/java/org/example/snsapp/domain/user/service/UserService.java
@@ -1,12 +1,22 @@
 package org.example.snsapp.domain.user.service;
 
 import lombok.RequiredArgsConstructor;
+import org.example.snsapp.domain.user.dto.UserBaseResponse;
+import org.example.snsapp.domain.user.entity.User;
 import org.example.snsapp.domain.user.repository.UserRepository;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 @Service
 @RequiredArgsConstructor
 public class UserService {
 
     private final UserRepository userRepository;
+
+    @Transactional(readOnly = true)
+    public UserBaseResponse getUserProfile(String email) {
+        User user = userRepository.findByEmailOrElseThrow(email);
+
+        return UserBaseResponse.create(user, 0, 0);
+    }
 }
