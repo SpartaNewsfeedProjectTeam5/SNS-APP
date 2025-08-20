6d39e8fedf579da68e30af4e8b5a1780f14f70cc
Hyjpark <hyej042@gmail.com>
2025-08-20 16:38:54 +0900

    feat: 프로필 조회 구현
    - UserBaseResponse DTO 반환
    - followerCount, followingCount는 팔로우 기능 미구현으로 임시 하드코딩 값 사용

--- src/main/java/org/example/snsapp/domain/user/controller/UserController.java
+++ src/main/java/org/example/snsapp/domain/user/controller/UserController.java
@@ -1,7 +1,11 @@
 package org.example.snsapp.domain.user.controller;
 
 import lombok.RequiredArgsConstructor;
+import org.example.snsapp.domain.user.dto.UserBaseResponse;
 import org.example.snsapp.domain.user.service.UserService;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -12,4 +16,9 @@ public class UserController {
 
     private final UserService userService;
 
+
+    @GetMapping("/users/{email}/profile")
+    public ResponseEntity<UserBaseResponse> getUserProfile(@PathVariable String email) {
+        return ResponseEntity.ok(userService.getUserProfile(email));
+    }
 }
--- src/main/java/org/example/snsapp/domain/user/repository/UserRepository.java
+++ src/main/java/org/example/snsapp/domain/user/repository/UserRepository.java
@@ -2,9 +2,16 @@ package org.example.snsapp.domain.user.repository;
 
 import org.example.snsapp.domain.user.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.server.ResponseStatusException;
 
 import java.util.Optional;
 
 public interface UserRepository extends JpaRepository<User, Long> {
-    Optional<User> findUserByEmail(String email);
+    Optional<User> findByEmail(String email);
+
+    default User findByEmailOrElseThrow(String email) {
+        return findByEmail(email).orElseThrow(() ->
+                new ResponseStatusException(HttpStatus.NOT_FOUND, "존재 하지 않는 사용자압니다."));
+    }
 }
