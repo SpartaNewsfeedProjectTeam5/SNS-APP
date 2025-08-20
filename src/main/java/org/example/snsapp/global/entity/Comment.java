8a3a446ccf5963c69637577742df7036ace0e104
LeeDongJae-KR <movementj98@gmail.com>
2025-08-20 10:35:44 +0900

    feat:Comment Enity 필드 정의

---
+++ src/main/java/org/example/snsapp/global/entity/Comment.java
@@ -0,0 +1,35 @@
+package org.example.snsapp.global.entity;
+
+import jakarta.persistence.*;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class Comment {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "user_id", nullable = false)
+    private String userId;
+
+    @Column(name = "comment", nullable = false, length = 500)
+    private String comment;
+
+    @Column(name = "created_at", nullable = false, updatable = false)
+    private LocalDateTime createdAt;
+
+    @Column(name = "modified_at", nullable = false)
+    private LocalDateTime modifiedAt;
+    public Comment(String userId, String comment) {
+        this.userId = userId;
+        this.comment = comment;
+    }
+
+}
