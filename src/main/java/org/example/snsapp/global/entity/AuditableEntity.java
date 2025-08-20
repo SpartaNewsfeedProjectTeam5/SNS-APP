e63960d2ed11be16baa96bb8da0eacf7f5e1c81f
kisu kim <qpsrltn@gmail.com>
2025-08-20 10:25:47 +0900

    feat: Post 엔티티, Like 엔티티, 공용 enum 타입, 공용 엔티티 추가 , 임시 User 엔티티 추가

--- src/main/java/org/example/snsapp/global/entity/AuditableEntity.java
+++ src/main/java/org/example/snsapp/global/entity/AuditableEntity.java
@@ -1,6 +1,5 @@
 package org.example.snsapp.global.entity;
 
-import jakarta.persistence.Column;
 import jakarta.persistence.MappedSuperclass;
 import lombok.Getter;
 import org.springframework.data.annotation.LastModifiedDate;
