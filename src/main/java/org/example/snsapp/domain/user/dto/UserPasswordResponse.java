db44754af8bec375c5ba411a732b8ee3ece50930
Hyjpark <hyej042@gmail.com>
2025-08-21 14:17:44 +0900

    feat: 로그인 유저 비밀번호 변경 기능 추가
    - 세션 기반 로그인 유저 인증 적용
    - 현재 비밀번호 확인 후 새로운 비밀번호로 변경

---
+++ src/main/java/org/example/snsapp/domain/user/dto/UserPasswordResponse.java
@@ -0,0 +1,23 @@
+package org.example.snsapp.domain.user.dto;
+
+import lombok.Builder;
+import lombok.Getter;
+import org.example.snsapp.domain.user.entity.User;
+
+import java.time.LocalDateTime;
+
+@Getter
+public class UserPasswordResponse {
+
+    private final String message;
+    private final LocalDateTime modifiedAt;
+
+    private UserPasswordResponse(String message, LocalDateTime modifiedAt) {
+        this.message = message;
+        this.modifiedAt = modifiedAt;
+    }
+
+    public static UserPasswordResponse ofSuccess(String message, LocalDateTime modifiedAt) {
+        return new UserPasswordResponse(message, modifiedAt);
+    }
+}
