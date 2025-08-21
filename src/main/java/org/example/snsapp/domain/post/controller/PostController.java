b625851a842747904453718190effefc2288f6af
kisu kim <qpsrltn@gmail.com>
2025-08-21 14:19:05 +0900

    feat: 게시물 검색 기능 추가

--- src/main/java/org/example/snsapp/domain/post/controller/PostController.java
+++ src/main/java/org/example/snsapp/domain/post/controller/PostController.java
@@ -5,16 +5,21 @@ import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.example.snsapp.domain.post.dto.PostBaseRequest;
 import org.example.snsapp.domain.post.dto.PostBaseResponse;
-import org.example.snsapp.domain.post.entity.Post;
+import org.example.snsapp.domain.post.dto.PostPageResponse;
 import org.example.snsapp.domain.post.service.PostService;
 import org.example.snsapp.global.enums.ErrorCode;
+import org.example.snsapp.global.enums.SearchType;
 import org.example.snsapp.global.exception.CustomException;
 import org.example.snsapp.global.util.SessionUtils;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
+import org.springframework.data.web.PageableDefault;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequiredArgsConstructor
@@ -30,4 +35,18 @@ public class PostController {
 
         return new ResponseEntity<>(postService.create(loginUserEmail, postBaseRequest), HttpStatus.CREATED);
     }
+
+    @GetMapping("/api/v1/posts/search")
+    public ResponseEntity<List<PostPageResponse>> search(@RequestParam String keyword,
+                                                         @RequestParam SearchType searchType,
+                                                         @PageableDefault(
+                                                                 page = 0,
+                                                                 size = 10,
+                                                                 sort = "create_at",
+                                                                 direction = Sort.Direction.DESC) Pageable pageable) {
+        Page<PostPageResponse> postPageResponse = postService.search(keyword, searchType, pageable);
+
+        return new ResponseEntity<>(postPageResponse.getContent(), HttpStatus.OK);
+    }
+
 }
--- src/main/java/org/example/snsapp/domain/post/service/PostService.java
+++ src/main/java/org/example/snsapp/domain/post/service/PostService.java
@@ -3,9 +3,16 @@ package org.example.snsapp.domain.post.service;
 import jakarta.validation.Valid;
 import org.example.snsapp.domain.post.dto.PostBaseRequest;
 import org.example.snsapp.domain.post.dto.PostBaseResponse;
+import org.example.snsapp.domain.post.dto.PostPageResponse;
+import org.example.snsapp.global.enums.SearchType;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.http.HttpStatusCode;
 
 import java.net.URI;
 
 public interface PostService {
     PostBaseResponse create(String loginUserEmail, @Valid PostBaseRequest postBaseRequest);
+
+    Page<PostPageResponse> search(String keyword, SearchType searchType, Pageable pageable);
 }
