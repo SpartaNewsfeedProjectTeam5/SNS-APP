ff05341da5b96587849f3d9026baaa2012f3f8b8
Hyjpark <hyej042@gmail.com>
2025-08-21 11:56:42 +0900

    feat: 세션 기반 로그인 유저 인증 적용하여 프로필 수정 API 개선

--- src/main/java/org/example/snsapp/domain/user/controller/UserController.java
+++ src/main/java/org/example/snsapp/domain/user/controller/UserController.java
@@ -9,6 +9,7 @@ import org.example.snsapp.domain.user.dto.UserBaseResponse;
 import org.example.snsapp.domain.user.dto.UserUpdateRequest;
 import org.example.snsapp.domain.user.dto.UserUpdateResponse;
 import org.example.snsapp.domain.user.service.UserService;
+import org.example.snsapp.global.constant.Const;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
@@ -29,8 +30,10 @@ public class UserController {
     }
 
     @PutMapping("/users/me/profile")
-    public ResponseEntity<UserUpdateResponse> updateUserProfile(@Valid @RequestBody UserUpdateRequest dto) {
-        //  TODO: 인증 정보로 로그인 사용자 ID 가져오기
-        return ResponseEntity.ok(userService.updateUserProfile(dto));
+    public ResponseEntity<UserUpdateResponse> updateUserProfile(
+            @Valid @RequestBody UserUpdateRequest dto,
+            @SessionAttribute(name = Const.LOGIN_USER) String email
+    ) {
+        return ResponseEntity.ok(userService.updateUserProfile(email, dto));
     }
 }
