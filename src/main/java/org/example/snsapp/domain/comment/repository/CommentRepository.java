fe514cddd6abb5dde007c4c7f93ccb0bf7cb3ff9
LeeDongJae-KR <movementj98@gmail.com>
2025-08-22 09:34:01 +0900

    댓글 기능 및 좋아요 기능 구현

--- src/main/java/org/example/snsapp/domain/comment/repository/CommentRepository.java
+++ src/main/java/org/example/snsapp/domain/comment/repository/CommentRepository.java
@@ -3,14 +3,18 @@ package org.example.snsapp.domain.comment.repository;
 import org.example.snsapp.domain.comment.entity.Comment;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
-import org.springframework.data.jpa.repository.EntityGraph;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 public interface CommentRepository extends JpaRepository<Comment, Long> {
 
     // 페이징 처리된 댓글 조회 (User 정보 포함)
-    @EntityGraph(attributePaths = {"user","post"})
-    Page<Comment> findByPostId(Long postId, Pageable pageable);
+    @Query("SELECT c FROM Comment c " +
+            "JOIN FETCH c.user " +
+            "WHERE c.post.id = :postId " +
+            "ORDER BY c.createdAt ASC")
+    Page<Comment> findByPostIdWithUser(@Param("postId") Long postId, Pageable pageable);
 
 }
 
